3e2d81999456b64e1627d35bc9e36373bad5f947
Hariprasad Natarajan <48129753+HariprasadKVN@users.noreply.github.com>
2024-04-01 11:24:19 +0530

    removed the unwated methods

--- routes/allocationRoutes.js
+++ routes/allocationRoutes.js
@@ -8,18 +8,7 @@ const {
 } = require("../controllers/allocationController");
 
 const router = express.Router();
-const fakeAuth = (p, req, res, next) => {
-  const { user } = req.params;
-  console.log(user);
-  next();
-};
-const test = (req, res, next) => {
-  console.log("test");
-  next("call fake");
-};
 
-router.use(test);
-router.use(fakeAuth);
 router
   .route("/users/:user/work/allocations")
   .get(getAllocations)
